41fa2a7bbddc2979e5c84a7c9c95b930a7003df0
Sankalp Sharma <sankalpshrm1111@gmail.com>
2023-10-17 00:37:16 +0530

    mini_project_4

--- mini_project_4/function.js
+++ mini_project_4/function.js
@@ -1,12 +1,12 @@
-const btn = document.getElementById("btt");
+const btn = document.getElementById("btt"); // this will select the id of the button
 
-const randomNumber = function(number){
-    return Math.floor(Math.random() * (number+1))
+const randomNumber = function(number){ // by this function we will get a randomNumber
+    return Math.floor(Math.random() * (number+1));
 } 
 
 
-btn.addEventListener("mouseover",()=>{
-    const rndCol = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`
-    document.body.style.backgroundColor = rndCol;
+btn.addEventListener("mouseover",()=>{ // this an evenListener which is made to react on the event happened in the system by the user
+    const rndCol = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`; //now that function we will use its output here to make a random color  
+    document.body.style.backgroundColor = rndCol; //now that random color will be in the background of the page
 
 });
