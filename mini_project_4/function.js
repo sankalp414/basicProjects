5fc9118b551cb4d6ba12b5df791fc0f22de03d64
Sankalp Sharma <sankalpshrm1111@gmail.com>
2023-10-18 00:12:48 +0530

    mini_project_4

--- mini_project_4/function.js
+++ mini_project_4/function.js
@@ -1,15 +1,16 @@
-const btn = document.getElementById("btt"); // this will select the id of the button
-const colorNum = document.getElementById("color")
-const randomNumber = function(number){ // by this function we will get a randomNumber
-    return Math.floor(Math.random() * (number+1));
-} 
+const button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
 
-
-btn.addEventListener("click",()=>{ // this an evenListener which is made to react on the event happened in the system by the user
-    const rndCol = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`; //now that function we will use its output here to make a random color  
-    document.body.style.backgroundColor = rndCol; //now that random color will be in the background of the page
-    console.log(rndCol);
-    colorNum.innerHTML = `${rndCol}`; // thiswill show the rgb value of the current color
-    //console.log(num);
-
+button.forEach((btn)=>{
+     btn.addEventListener('click',function(e){
+         if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id;
+         } if(e.target.id === "red"){
+            body.style.backgroundColor = e.target.id;
+         } if(e.target.id === "blue"){
+            body.style.backgroundColor = e.target.id;
+         } if(e.target.id === "purple"){
+            body.style.backgroundColor = e.target.id;
+         }
+     });
 });
