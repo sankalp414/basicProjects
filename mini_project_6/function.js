476bf85546437dd5a3c4a00e3ec7040ca53dfd1a
Sankalp Sharma <sankalpshrm1111@gmail.com>
2023-11-15 23:50:19 +0530

    mini project 6

---
+++ mini_project_6/function.js
@@ -0,0 +1,53 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.calculator button');
+
+let currentInput = '';
+let currentOperator = '';
+let currentResult = null;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const value = button.textContent;
+
+        if (button.hasAttribute('data-number')) {
+            currentInput += value;
+            display.value = currentInput;
+        } else if (button.hasAttribute('data-operator')) {
+            if (value === '=') {
+                if (currentOperator && currentInput !== '') {
+                    currentResult = operate(currentResult, parseFloat(currentInput), currentOperator);
+                    display.value = currentResult;
+                    currentInput = '';
+                    currentOperator = '';
+                }
+            } else if (value === 'C') {
+                currentInput = '';
+                currentOperator = '';
+                currentResult = null;
+                display.value = '';
+            } else {
+                if (currentOperator && currentInput !== '') {
+                    currentResult = operate(currentResult, parseFloat(currentInput), currentOperator);
+                    display.value = currentResult;
+                    currentInput = '';
+                }
+                currentOperator = value;
+            }
+        }
+    });
+});
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        default:
+            return b;
+    }
+}
