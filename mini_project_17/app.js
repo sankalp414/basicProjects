51e86f94f7938fdb8f62a2002dc6663f1db2a3ac
Sankalp Sharma <sankalpshrm1111@gmail.com>
2024-01-07 23:23:46 +0530

    mini project 17

--- mini_project_17/app.js
+++ mini_project_17/app.js
@@ -1,10 +1,22 @@
 //movement animation
 
-const card = document.querySelector('.card');
-const container = document.querySelector('.container');
+const card = document.querySelector('.card')
+const container = document.querySelector('.container')
 
 container.addEventListener('mousemove',(e)=>{
-    let xAxis = ((window.innerWidth/2)-e.pageX);
-    let yAxis = ((window.innerHeight/2)-e.pageY);
-    
+  let xAxis = ((window.innerWidth/2)-e.pageX)/10;
+  let yAxis = ((window.innerHeight/2)-e.pageY)/10;
+  console.log(xAxis,yAxis);
+   card.style.transform = `rotateX(${xAxis}deg) rotateY(${yAxis}deg)`;
+})
+//animate in
+
+container.addEventListener('mouseenter',()=>{
+    card.style.transition = 'none'
+})
+
+//animate out
+container.addEventListener('mouseleave',()=>{
+    card.style.transition = 'all 0.5s ease'
+    card.style.transform = `rotateX(0deg) rotateY(0deg)`;
 })
